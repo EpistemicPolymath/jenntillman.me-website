053d0112a8332430927bac2af03ef9533ae8aebb
Dishit-Dhanesha <dishitrvdhanesha@gmail.com>
2021-10-30 14:38:16 +0530

    LOgo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -22,7 +22,7 @@
     <header id="header" class="hoc clear">
       <div id="logo" class="fl_left"> 
         <!-- ################################################################################################ -->
-        <h1><a href="index.html">Yammoe</a></h1>
+        <h1><a href="index.html">An Epistemic Polymath</a></h1>
         <!-- ################################################################################################ -->
       </div>
       <nav id="mainav" class="fl_right"> 
@@ -371,7 +371,7 @@
     <div id="copyright" class="hoc clear"> 
       <!-- ################################################################################################ -->
       <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">Domain Name</a></p>
-      <p class="fl_right">Template by <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+      <p class="fl_right">Template by <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">Royale Group</a></p>
       <!-- ################################################################################################ -->
     </div>
   </div>
